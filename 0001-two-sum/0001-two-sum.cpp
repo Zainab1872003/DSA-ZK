f72f2e2bd69618bf3bf50dd1305928f319aeae30
Zainab1872003 <151448811+Zainab1872003@users.noreply.github.com>
2025-02-26 16:28:17 +0500

    Time: 0 ms (100%), Space: 14.7 MB (54.96%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int>temp;
-        for(int i = 0 ;i<nums.size();i++){
-            for(int j = i+1;j<nums.size();j++){
-                if(nums[i]+nums[j]==target){
-                    return {i,j};
-                   
-                }
-            }
+          vector<pair<int, int>> num_indices;
+    for (int i = 0; i < nums.size(); ++i) {
+        num_indices.push_back({nums[i], i}); 
+    }
+    sort(num_indices.begin(), num_indices.end());
+
+    int left = 0, right = nums.size() - 1;
+
+    while (left < right) {
+        int sum = num_indices[left].first + num_indices[right].first;
+
+        if (sum == target) {
+            return {num_indices[left].second, num_indices[right].second};
+        } else if (sum < target) {
+            ++left; // Move left pointer to the right
+        } else {
+            --right; // Move right pointer to the left
         }
-        return {};
+    }
+
+    return {};
+        
     }
 };
